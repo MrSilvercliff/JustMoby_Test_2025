09edf297e38417a27eec411eee38f6dfe6021697
Mikhail <>
2025-10-01 15:16:51 +0300

    - пустой скрипт инсталлера игровой сцены

---
+++ JustMoby_Test_2025/Assets/_Project/Scripts/GameScene/Zenject/GameSceneInstaller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace _Project.Scripts.GameScene.Zenject
+{
+    public class GameSceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
