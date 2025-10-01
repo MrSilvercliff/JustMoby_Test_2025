2b8d8501bc9545b80b315068ece181696d5761ff
Mikhail <>
2025-10-01 15:16:18 +0300

    - пустой скрипт прожект инсталлера

---
+++ JustMoby_Test_2025/Assets/_Project/Scripts/Project/Zenject/ProjectInstaller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace _Project.Scripts.Project.Zenject
+{
+    public class ProjectInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
