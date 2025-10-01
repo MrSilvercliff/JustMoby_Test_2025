3fd35ac112c2a1003ebce5ffedcaebbd985ac635
Mikhail <>
2025-10-01 16:39:55 +0300

    - пустой контроллер сцены игры с кубиками

---
+++ JustMoby_Test_2025/Assets/_Project/Scripts/BrickTowerGameScene/Scene/BrickTowerGameSceneController.cs
@@ -0,0 +1,30 @@
+using _Project.Scripts.Project.Scenes;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace _Project.Scripts.BrickTowerGameScene.Scene
+{
+    public class BrickTowerGameSceneController : SceneController
+    {
+        protected override Task OnAwake()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override Task OnStart()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override Task OnLateStart()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override void OnFlush()
+        {
+        }
+    }
+}
