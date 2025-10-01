46205883a5051850f5d156c53090131f5a43bd1e
Mikhail <>
2025-10-01 15:27:43 +0300

    - поправил наследование

--- JustMoby_Test_2025/Assets/_Project/Scripts/GameScene/Zenject/GameSceneInstaller.cs
+++ JustMoby_Test_2025/Assets/_Project/Scripts/GameScene/Zenject/GameSceneInstaller.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
+using ZerglingUnityPlugins.ZenjectExtentions.SceneInstallers;
 
 namespace _Project.Scripts.GameScene.Zenject
 {
-    public class GameSceneInstaller : MonoInstaller
+    public class GameSceneInstaller : SceneInstallerBasic
     {
         public override void InstallBindings()
         {
