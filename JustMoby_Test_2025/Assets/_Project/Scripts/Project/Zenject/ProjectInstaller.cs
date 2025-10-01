40709829944cf9787c53606eb2f63d75379faa9a
Mikhail <>
2025-10-01 15:27:55 +0300

    - бинд провайдера контекста

--- JustMoby_Test_2025/Assets/_Project/Scripts/Project/Zenject/ProjectInstaller.cs
+++ JustMoby_Test_2025/Assets/_Project/Scripts/Project/Zenject/ProjectInstaller.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
+using ZerglingUnityPlugins.ZenjectExtentions.ContextProvider;
 
 namespace _Project.Scripts.Project.Zenject
 {
     public class ProjectInstaller : MonoInstaller
     {
         public override void InstallBindings()
+        {
+            BindBasicServices();
+            BindProjectServices();
+        }
+
+        private void BindBasicServices()
+        {
+            BindZenjectExtensions();
+        }
+
+        private void BindZenjectExtensions()
+        {
+            Container.Bind<IZenjectContextProvider>().To<ZenjectContextProvider>().AsSingle();
+        }
+
+        private void BindProjectServices()
         {
         }
     }
