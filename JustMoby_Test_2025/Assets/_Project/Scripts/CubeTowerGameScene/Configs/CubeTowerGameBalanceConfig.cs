dce7db668c9fd2d1b34b2148e7318532209a4fc1
Mikhail <>
2025-10-02 21:34:13 +0300

    - баланс конфиг игры про башню кубиков

--- JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Configs/CubeTowerGameBalanceConfig.cs
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Configs/CubeTowerGameBalanceConfig.cs
@@ -1,3 +1,5 @@
+using _Project.Scripts.Project.Configs;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +8,14 @@ namespace _Project.Scripts.CubeTowerGameScene.Configs
 {
     public interface ICubeTowerGameBalanceConfig
     {
+        ICubeBalanceConfig CubeBalanceConfig { get; }
     }
 
-    [CreateAssetMenu(fileName = "CubeTowerGameBalanceConfig", menuName = "Project/Configs/Brick Tower Game/Cube Tower Game Balance Config")]
+    [CreateAssetMenu(fileName = "CubeTowerGameBalanceConfig", menuName = "Project/Configs/Cube Tower Game/Cube Tower Game Balance Config")]
     public class CubeTowerGameBalanceConfig : ScriptableObject, ICubeTowerGameBalanceConfig
     {
+        public ICubeBalanceConfig CubeBalanceConfig => _cubeBalanceConfig;
 
+        [SerializeField] private CubeBalanceConfig _cubeBalanceConfig;
     }
 }
