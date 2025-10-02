161fd1fe29fad945f7e9f17e7f83b127cf3ea513
Mikhail <>
2025-10-02 23:16:20 +0300

    - бинд баланс сервиса сцены и объектного пула кубиков

--- JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Zenject/CubeTowerGameSceneInstaller.cs
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Zenject/CubeTowerGameSceneInstaller.cs
@@ -1,3 +1,7 @@
+using _Project.Scripts.CubeTowerGameScene.Configs;
+using _Project.Scripts.CubeTowerGameScene.Scene;
+using _Project.Scripts.CubeTowerGameScene.Services.Balance;
+using _Project.Scripts.CubeTowerGameScene.UI.Cubes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,8 +12,39 @@ namespace _Project.Scripts.CubeTowerGameScene.Zenject
 {
     public class CubeTowerGameSceneInstaller : SceneInstallerBasic
     {
+        [SerializeField] private CubeTowerGameSceneObjectPoolContainer _objectPoolContainer;
+        [SerializeField] private CubeTowerGameBalanceConfig _balanceConfig;
+
         public override void InstallBindings()
         {
+            BindObjectPools();
+
+            BindBalanceServices();
+        }
+
+        private void BindBalanceServices()
+        {
+            Container.Bind<ICubeTowerGameBalanceConfig>().FromInstance(_balanceConfig).AsSingle();
+            Container.Bind<ICubeTowerGameBalanceService>().To<CubeTowerGameBalanceService>().AsSingle();
+        }
+
+        private void BindObjectPools()
+        {
+            BindCubeWidgetObjectPool();
+        }
+
+        private void BindCubeWidgetObjectPool()
+        {
+            var poolItem = _objectPoolContainer.CubeWidget;
+
+            var prefab = poolItem.Prefab;
+            var container = poolItem.Container;
+            var poolInitSize = poolItem.PoolInitialSize;
+
+            Container.BindMemoryPool<CubeWidget, CubeWidget.Pool>()
+                .WithInitialSize(poolInitSize)
+                .FromComponentInNewPrefab(prefab)
+                .UnderTransform(container);
         }
     }
 }
