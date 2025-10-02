2f73abd40f1ad1990793044e9e5a9c3fae5dea41
Mikhail <>
2025-10-02 23:48:53 +0300

    - пустой набросок игрового вью окна сцены

---
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/UI/Windows/Views/GameView.cs
@@ -0,0 +1,26 @@
+using _Project.Scripts.CubeTowerGameScene.UI.Cubes;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using ZerglingUnityPlugins.WindowsManagerAsync.Scripts.Views;
+
+namespace _Project.Scripts.CubeTowerGameScene.UI.Windows.Views
+{
+    public struct TestStruct
+    { 
+    }
+
+    public class GameView : ViewWindowWithSafeArea
+    {
+        [SerializeField] private CubeScrollWidget _cubeScrollWidget;
+
+        protected override async Task<bool> OnInit()
+        {
+            await base.OnInit();
+            _cubeScrollWidget.Init();
+            return true;
+        }
+    }
+}
