57ccecbc4d9125cd086e1a2c1ff2c5c20133a5ca
Mikhail <>
2025-10-02 23:15:14 +0300

    - делаю виджет кубика

---
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/UI/Cubes/CubeWidget.cs
@@ -0,0 +1,57 @@
+using _Project.Scripts.CubeTowerGameScene.Services.Balance.Models;
+using _Project.Scripts.Project.ObjectPools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace _Project.Scripts.CubeTowerGameScene.UI.Cubes
+{
+    public class CubeWidget : MonoBehaviour, IProjectPoolable
+    {
+        [SerializeField] private Image _image;
+
+        private ICubeBalanceModel _balanceModel;
+
+        public void Setup(ICubeBalanceModel cubeBalanceModel)
+        { 
+            _balanceModel = cubeBalanceModel;
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            if (_balanceModel == null)
+                OnRefreshNull();
+            else
+                OnRefreshNotNull();
+        }
+
+        private void OnRefreshNull()
+        {
+            _image.sprite = null;
+        }
+
+        private void OnRefreshNotNull()
+        {
+            _image.sprite = _balanceModel.Sprite;
+        }
+
+        public void OnCreated()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void OnDespawned()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void OnSpawned()
+        {
+        }
+
+        public class Pool : ProjectMonoMemoryPool<CubeWidget> { }
+    }
+}
