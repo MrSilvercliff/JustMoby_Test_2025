c231b98278ac31b93ef42ea0d9e9a4b73e0f0411
Mikhail <>
2025-10-02 22:21:21 +0300

    - добавил баланс сторадж кубиков в сервис баланса

--- JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Services/Balance/CubeTowerGameBalanceService.cs
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Services/Balance/CubeTowerGameBalanceService.cs
@@ -1,24 +1,33 @@
+using _Project.Scripts.CubeTowerGameScene.Services.Balance.Storages;
 using _Project.Scripts.Project.Services.Balance;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 using ZerglingUnityPlugins.Tools.Scripts.Interfaces.ProjectService.AsyncSync;
 
 namespace _Project.Scripts.CubeTowerGameScene.Services.Balance
 {
     public interface ICubeTowerGameBalanceService : IBalanceService
     {
+        public ICubeBalanceStorage Cubes { get; }
     }
 
     public class CubeTowerGameBalanceService : BalanceService, ICubeTowerGameBalanceService
     {
+        public ICubeBalanceStorage Cubes => _cubeBalanceStorage;
+
+        private ICubeBalanceStorage _cubeBalanceStorage;
+
         public CubeTowerGameBalanceService()
         {
+            _cubeBalanceStorage = new CubeBalanceStorage();
         }
 
         protected override HashSet<IProjectService> GetStoragesToInit()
         {
             var result = new HashSet<IProjectService>();
+            result.Add(_cubeBalanceStorage);
             return result;
         }
     }
