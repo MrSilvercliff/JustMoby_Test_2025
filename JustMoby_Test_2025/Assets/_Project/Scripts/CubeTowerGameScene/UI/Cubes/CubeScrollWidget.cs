29f955dcbe5125fce00775dacc63a2f2f495783d
Mikhail <>
2025-10-03 00:47:13 +0300

    - делаю виджет скролла кубиков

---
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/UI/Cubes/CubeScrollWidget.cs
@@ -0,0 +1,43 @@
+using _Project.Scripts.CubeTowerGameScene.Services.Balance;
+using _Project.Scripts.Project.Extensions;
+using _Project.Scripts.Project.Services.ObjectPools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace _Project.Scripts.CubeTowerGameScene.UI.Cubes
+{
+    public class CubeScrollWidget : MonoBehaviour
+    {
+        [SerializeField] private Transform _scrollViewContent;
+
+        [Inject] private ICubeTowerGameBalanceService _balanceService;
+        [Inject] private IProjectObjectPoolService _objectPoolService;
+
+        private List<CubeWidget> _widgets;
+
+        public void Init()
+        {
+            _widgets = new();
+        }
+
+        public void SpawnCubeWidgets()
+        {
+            var cubeWidgetPool = _objectPoolService.GetObjectPool<CubeWidget.Pool>();
+            var activeBalanceModels = _balanceService.Cubes.GetActiveCubeBalanceModels();
+
+            foreach (var cubeBalanceModel in activeBalanceModels) 
+            {
+                var newWidget = cubeWidgetPool.Spawn();
+                newWidget.Setup(cubeBalanceModel);
+                newWidget.transform.SetParent(_scrollViewContent);
+                newWidget.transform.ResetLocalPosition();
+                newWidget.transform.ResetLocalRotation();
+                newWidget.transform.ResetLocalScale();
+
+                _widgets.Add(newWidget);
+            }
+        }
+    }
+}
