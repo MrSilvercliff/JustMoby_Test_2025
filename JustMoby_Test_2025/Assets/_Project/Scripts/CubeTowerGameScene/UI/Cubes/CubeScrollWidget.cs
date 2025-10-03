5628e502f5d1a7109d59cb23d56c20e4b811c892
Mikhail <>
2025-10-03 22:10:38 +0300

    - делаю проверку на необходимость скролла

--- JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/UI/Cubes/CubeScrollWidget.cs
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/UI/Cubes/CubeScrollWidget.cs
@@ -11,8 +11,15 @@ namespace _Project.Scripts.CubeTowerGameScene.UI.Cubes
 {
     public class CubeScrollWidget : MonoBehaviour
     {
+        [SerializeField] private GameObject _scrollRectGO;
         [SerializeField] private ScrollRect _scrollRect;
-        [SerializeField] private Transform _scrollViewContent;
+        [SerializeField] private RectTransform _scrollRectContent;
+        
+        [SerializeField] private GameObject _nonScrollRectGO;
+        [SerializeField] private RectTransform _nonScrollContent;
+
+        [SerializeField] private HorizontalLayoutGroup _layout;
+        [SerializeField] private RectTransform _prefabTransform;
 
         [Inject] private ICubeTowerGameBalanceService _balanceService;
         [Inject] private ICubeTowerGameSceneObjectPoolService _objectPoolService;
@@ -26,21 +33,38 @@ namespace _Project.Scripts.CubeTowerGameScene.UI.Cubes
 
         public void SpawnCubeWidgets()
         {
-            var cubeWidgetPool = _objectPoolService.CubeScrollDraggableWidget; ;
+            var cubeWidgetPool = _objectPoolService.CubeScrollDraggableWidgetPool;
             var activeBalanceModels = _balanceService.Cubes.GetActiveCubeBalanceModels();
+
+            var scrollNeeded = CheckScrollNeeded(activeBalanceModels.Count);
+
+            _scrollRectGO.SetActive(scrollNeeded);
+            _nonScrollRectGO.SetActive(!scrollNeeded);
+            var cubeWidgetsContainer = scrollNeeded ? _scrollRectContent : _nonScrollContent;
 
             foreach (var cubeBalanceModel in activeBalanceModels) 
             {
                 var newWidget = cubeWidgetPool.Spawn();
                 newWidget.Setup(cubeBalanceModel);
                 newWidget.Setup(_scrollRect);
-                newWidget.transform.SetParent(_scrollViewContent);
+                newWidget.transform.SetParent(cubeWidgetsContainer);
                 newWidget.transform.ResetLocalPosition();
                 newWidget.transform.ResetLocalRotation();
                 newWidget.transform.ResetLocalScale();
 
                 _widgets.Add(newWidget);
             }
+        }
+
+        private bool CheckScrollNeeded(int cubesCount)
+        {
+            var layoutPadding = _layout.padding.left + _layout.padding.right;
+            var layoutSpacing = _layout.spacing;
+            var prefabWidth = _prefabTransform.rect.width;
+            var itemsSumWidth = (prefabWidth * cubesCount) + (layoutSpacing * (cubesCount - 1));
+            var sumWidth = itemsSumWidth + layoutPadding;
+            var result = sumWidth > _nonScrollContent.rect.width;
+            return result;
         }
     }
 }
