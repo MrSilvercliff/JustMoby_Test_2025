961c0adc1f78991a7ef231bf74e4c6563acf5880
Mikhail <>
2025-10-03 19:01:12 +0300

    - фикс неймспейсов

--- JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/UI/Cubes/CubeDraggableWidget.cs
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/UI/Cubes/CubeDraggableWidget.cs
@@ -1,9 +1,7 @@
-using _Project.Scripts.CubeTowerGameScene.Enums;
 using _Project.Scripts.Project.ObjectPools;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 namespace _Project.Scripts.CubeTowerGameScene.UI.Cubes
 {
