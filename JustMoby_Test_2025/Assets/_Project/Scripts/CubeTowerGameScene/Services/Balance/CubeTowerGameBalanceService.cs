9a33382c755c6882d17cba760a48bd702e432668
Mikhail <>
2025-10-03 18:56:21 +0300

    - добавил баланс сторадж драг-н-дропа в сервис баланса

--- JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Services/Balance/CubeTowerGameBalanceService.cs
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Services/Balance/CubeTowerGameBalanceService.cs
@@ -10,24 +10,29 @@ namespace _Project.Scripts.CubeTowerGameScene.Services.Balance
 {
     public interface ICubeTowerGameBalanceService : IBalanceService
     {
-        public ICubeBalanceStorage Cubes { get; }
+        ICubeBalanceStorage Cubes { get; }
+        ICubeDragAndDropBalanceStorage CubeDragAndDropBalanceStorage { get; }
     }
 
     public class CubeTowerGameBalanceService : BalanceService, ICubeTowerGameBalanceService
     {
         public ICubeBalanceStorage Cubes => _cubeBalanceStorage;
+        public ICubeDragAndDropBalanceStorage CubeDragAndDropBalanceStorage => _cubeDragAndDropBalanceStorage;
 
         private ICubeBalanceStorage _cubeBalanceStorage;
+        private ICubeDragAndDropBalanceStorage _cubeDragAndDropBalanceStorage;
 
         public CubeTowerGameBalanceService()
         {
             _cubeBalanceStorage = new CubeBalanceStorage();
+            _cubeDragAndDropBalanceStorage = new CubeDragAndDropBalanceStorage();
         }
 
         protected override HashSet<IProjectService> GetStoragesToInit()
         {
             var result = new HashSet<IProjectService>();
             result.Add(_cubeBalanceStorage);
+            result.Add(_cubeDragAndDropBalanceStorage);
             return result;
         }
     }
