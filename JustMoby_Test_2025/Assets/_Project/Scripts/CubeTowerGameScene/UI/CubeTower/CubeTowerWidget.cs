053e4dd7cd11ab9cc45c02d085cc95a3fdcb668c
Mikhail <>
2025-10-05 00:52:16 +0300

    - делаю виджет башни кубиков

--- JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/UI/CubeTower/CubeTowerWidget.cs
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/UI/CubeTower/CubeTowerWidget.cs
@@ -1,3 +1,4 @@
+using _Project.Scripts.Project.Extensions;
 using _Project.Scripts.Project.ObjectPools;
 using _Project.Scripts.Project.UI;
 using System.Collections;
@@ -7,16 +8,21 @@ using UnityEngine;
 namespace _Project.Scripts.CubeTowerGameScene.UI.CubeTower
 {
     public interface ICubeTowerWidget : IProjectPoolable
-    { 
+    {
+        void AddCube(CubeTowerCubeWidget cubeWidget);
+        void RemoveCube(CubeTowerCubeWidget cubeWidget);
     }
 
     public class CubeTowerWidget : UIWidget, ICubeTowerWidget
     {
-        [SerializeField] private Transform _towerCubeContainer;
+        [SerializeField] private Transform _cubeContainer;
+
+        private List<CubeTowerCubeWidget> _cubes;
 
         public void OnCreated()
         {
             SetActive(false);
+            _cubes = new();
         }
 
         public void OnSpawned()
@@ -27,6 +33,22 @@ namespace _Project.Scripts.CubeTowerGameScene.UI.CubeTower
         public void OnDespawned()
         {
             SetActive(false);
+            _cubes.Clear();
+        }
+
+        public void AddCube(CubeTowerCubeWidget cubeWidget)
+        {
+            _cubes.Add(cubeWidget);
+            cubeWidget.transform.SetParent(_cubeContainer);
+            cubeWidget.transform.ResetLocalPosition();
+            cubeWidget.transform.ResetLocalRotation();
+            cubeWidget.transform.ResetLocalScale();
+            cubeWidget.SetActive(true);
+        }
+
+        public void RemoveCube(CubeTowerCubeWidget cubeWidget)
+        {
+            _cubes.Remove(cubeWidget);
         }
 
         public class Pool : ProjectMonoMemoryPool<CubeTowerWidget> { }
