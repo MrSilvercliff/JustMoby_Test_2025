c3d85b5c112e2a812d4a338d5234ea994b870bb6
Mikhail <>
2025-10-04 13:59:39 +0300

    - добавил пул башни кубиков в сервис пулов

--- JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Services/ObjectPools/CubeTowerGameSceneObjectPoolService.cs
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Services/ObjectPools/CubeTowerGameSceneObjectPoolService.cs
@@ -1,4 +1,5 @@
 using _Project.Scripts.CubeTowerGameScene.UI.Cubes;
+using _Project.Scripts.CubeTowerGameScene.UI.CubeTower;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,14 +11,17 @@ namespace _Project.Scripts.CubeTowerGameScene.Services.ObjectPools
     { 
         CubeWidget.Pool CubeWidgetPool { get; }
         CubeScrollDraggableWidget.Pool CubeScrollDraggableWidgetPool { get; }
+        CubeTowerWidget.Pool CubeTowerWidgetPool { get; }
     }
 
     public class CubeTowerGameSceneObjectPoolService : ICubeTowerGameSceneObjectPoolService
     {
         public CubeWidget.Pool CubeWidgetPool => _cubeWidgetPool;
         public CubeScrollDraggableWidget.Pool CubeScrollDraggableWidgetPool => _cubeScrollDraggableWidgetPool;
+        public CubeTowerWidget.Pool CubeTowerWidgetPool => _cubeTowerWidgetPool;
 
         [Inject] private CubeWidget.Pool _cubeWidgetPool;
         [Inject] private CubeScrollDraggableWidget.Pool _cubeScrollDraggableWidgetPool;
+        [Inject] private CubeTowerWidget.Pool _cubeTowerWidgetPool;
     }
 }
