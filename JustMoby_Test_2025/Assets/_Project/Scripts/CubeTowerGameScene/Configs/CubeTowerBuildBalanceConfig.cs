5d880a77357261663f539466e87e82e31af0a757
Mikhail <>
2025-10-04 14:12:09 +0300

    - конфиг баланса постройки башен кубиков

--- JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Configs/CubeTowerBuildBalanceConfig.cs
+++ JustMoby_Test_2025/Assets/_Project/Scripts/CubeTowerGameScene/Configs/CubeTowerBuildBalanceConfig.cs
@@ -6,10 +6,14 @@ namespace _Project.Scripts.CubeTowerGameScene.Configs
 {
     public interface ICubeTowerBuildBalanceConfig
     { 
+        int MaxActiveTowerCount { get; }
     }
 
     [CreateAssetMenu(fileName = "CubeTowerBuildBalanceConfig", menuName = "Project/Configs/Cube Tower Game/Cube Tower Build Balance Config")]
     public class CubeTowerBuildBalanceConfig : ScriptableObject, ICubeTowerBuildBalanceConfig
     {
+        public int MaxActiveTowerCount => _maxActiveTowerCount;
+
+        [SerializeField] private int _maxActiveTowerCount;
     }
 }
