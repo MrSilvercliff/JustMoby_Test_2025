1e0d4b3aeaaecb3a7f1f79d05aae6f27e659e011
Mikhail <>
2025-10-05 22:34:39 +0300

    - добавил плагин локализации

---
+++ JustMoby_Test_2025/Assets/Plugins/ZerglingUnityPlugins/Localization_JSON_Object/Editor/LocalizationDownloadConfigEditor.cs
@@ -0,0 +1,37 @@
+﻿using ZerglingUnityPlugins.Localization_JSON_Object.Scripts;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+namespace ZerglingUnityPlugins.Localization_JSON_Object.Editor
+{
+    [CustomEditor(typeof(LocalizationDownloadConfig))]
+    public class LocalizationDownloadConfigEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("DOWNLOAD"))
+                OnDownloadButtonClick();
+
+            if (GUILayout.Button("TEST"))
+                OnTestButtonClick();
+        }
+
+        private void OnDownloadButtonClick()
+        {
+            var config = (ILocalizationDownloadConfig)target;
+            config.Download();
+        }
+
+        private void OnTestButtonClick()
+        { 
+            var config = (ILocalizationDownloadConfig)target;
+            config.Test();
+        }
+    }
+}
+
+#endif
